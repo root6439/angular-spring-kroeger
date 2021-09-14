3a699af16bd0d65943f42c7b5f5e9de41703deef
Nicolas Soares Henrique <nicolas.soaresh.98@gmail.com>
2021-09-14 18:00:00 -0300

    Primeiro commit, alguns componentes e módulos criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminComponentComponent } from './admin-component/admin-component.component';
+
+
+
+@NgModule({
+  declarations: [
+    AdminComponentComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
